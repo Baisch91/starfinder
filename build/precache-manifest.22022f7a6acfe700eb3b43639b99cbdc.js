5664ce8f3e59ffc56f3a8b34bbfbef96a51e4ea4
Chuck Baisch <58837693+Baisch91@users.noreply.github.com>
2020-06-02 18:41:35 -0400

    Add files via upload

---
+++ build/precache-manifest.22022f7a6acfe700eb3b43639b99cbdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c7420206e786ccd4cea433358fab2e",
+    "url": "/starfinder/index.html"
+  },
+  {
+    "revision": "dae0259094d4daf5f565",
+    "url": "/starfinder/static/css/main.634ac05c.chunk.css"
+  },
+  {
+    "revision": "e323ec2866ebe6106821",
+    "url": "/starfinder/static/js/2.b097e029.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starfinder/static/js/2.b097e029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae0259094d4daf5f565",
+    "url": "/starfinder/static/js/main.31d3fb44.chunk.js"
+  },
+  {
+    "revision": "8f63da2ec02bda2b737e",
+    "url": "/starfinder/static/js/runtime-main.7956d615.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/starfinder/static/media/Starjedi.cc3019aa.ttf"
+  }
+]);
